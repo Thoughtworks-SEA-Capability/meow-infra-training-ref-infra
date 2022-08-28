82be6ae4716e4ab6d737e920de1ea8dbf8720b60
James Koh <jameskohyz@gmail.com>
2022-08-28 20:43:41 +0800

    Add outputs to network

---
+++ week2/stacks/networking/outputs.tf
@@ -0,0 +1,12 @@
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+output "database_subnets" {
+  value = module.vpc.database_subnets
+}
+output "database_subnet_group_name" {
+  value = module.vpc.database_subnet_group_name
+}
